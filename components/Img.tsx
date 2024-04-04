1cc25e597c09f4060e833bf56226b9385b02c07f
ALEpantovic <129178866+ALEpantovic@users.noreply.github.com>
2024-04-04 20:46:36 +0200

    Update Img.tsx

--- components/Img.tsx
+++ components/Img.tsx
@@ -1,14 +1,14 @@
-
-import Image  from 'next/image';
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Img = () => {
   return (
     <div className="relative w-full max-container padding-container  flex flex-col gap-20 md:gap-28">
       <Image
-        src="bgImg.png"
-        alt="Going Medieval background"
-        layout="responsive"
+        src={bgImg.png}
+        alt={Going Medieval background}
+        layout={responsive}
         width={500} 
         height={400}
         className="rounded-md sm:w-full md:w-full lg:w-full xl:w-1/2"
