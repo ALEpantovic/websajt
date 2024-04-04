f24823b2699563d8aa45162a5d62178acd3324b5
ALEpantovic <129178866+ALEpantovic@users.noreply.github.com>
2024-04-04 20:03:06 +0200

    Update Img.tsx

--- components/Img.tsx
+++ components/Img.tsx
@@ -6,7 +6,7 @@ const Img = () => {
   return (
     <div className="relative w-full max-container padding-container  flex flex-col gap-20 md:gap-28">
       <Image
-        src="./public/bgImg.png"
+        src={reddit.svg}
         alt="Going Medieval background"
         layout="responsive"
         width={500} 
