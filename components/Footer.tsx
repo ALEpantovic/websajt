8704c88e398fd56d52ff8d39616735f5ac4454ed
ALEpantovic <pantovicaleksandar2000@gmail.com>
2024-04-05 15:37:29 +0200

    fixed footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Image from 'next/image';
 import { NAV_LINKS, MEDIA } from "@/constants";
 import Link from 'next/link';
@@ -10,15 +9,15 @@ const Footer = () => {
         <div className="flex justify-center space-x-10">
           {MEDIA.map((media) => (
             <Link href={media.link} target="_blank" key={media.key} className="flex items-center justify-center">
-                <Image src={media.image} alt={media.key} width={50} height={50} />
+                <Image src={media.image} alt={media.key} width={50} height={50} className="transition duration-300 hover:scale-110"/>
             </Link>
           ))}
         </div>
         <div className="flex flex-wrap justify-between items-center">
-          <ul className="gap-20 flex flex-row xs:gap-1 sm:gap-1 items-center">
+ 	<ul className="gap-20 flex flex-row xs:hidden sm:hidden items-center md:hidden lg:flex">
             {NAV_LINKS.map((link) => (
               <Link href={link.href} key={link.key} className="text-xl regular-16 text-white flexCenter cursor-pointer p-3 transition-all hover:font-bold">
-                 <p className='p-5 text-xl border-r-2'>{link.label}</p>
+                <p className='p-5 text-xl border-r-2'>{link.label}</p>
               </Link>
             ))}
           </ul>
