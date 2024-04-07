3241fc02570d6361a48c5d1eb429dd75b0f1fe7a
ALEpantovic <129178866+ALEpantovic@users.noreply.github.com>
2024-04-07 19:45:49 +0200

    Update 404.tsx

--- components/404.tsx
+++ components/404.tsx
@@ -1,11 +1,13 @@
 'use client'
 import React, { useEffect } from 'react';
 
-const VALID_URLS = ['websajt', 'websajt/Jobs', 'websajt/Contact', 'websajt/Game', 'websajt/Press']; 
+const VALID_URLS = ['websajt', 'websajt/Jobs', 'websajt/Contact', 'websajt/Game', 'websajt/Press'];
+
 const RedirectToHomeIfInvalid = () => {
  useEffect(() => {
     const currentPath = window.location.pathname;
-    if (!VALID_URLS.includes(currentPath)) {
+    // Check if the current path is not exactly '/websajt'
+    if (currentPath !== '/websajt' && !VALID_URLS.includes(currentPath)) {
       window.location.replace('/websajt');
     }
  }, []);
