c0e357dbd841439d74997546636b52816fdd0910
ALEpantovic <129178866+ALEpantovic@users.noreply.github.com>
2024-04-11 20:18:16 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import { FEATURES } from '@/constants';
 import Image from 'next/image';
 import React, { useState, useEffect, useRef } from 'react';
@@ -7,7 +6,7 @@ import React, { useState, useEffect, useRef } from 'react';
 const Hero = () => {
  const [animatedFeatures, setAnimatedFeatures] = useState<string[]>([]);
  const ulRef = useRef<HTMLUListElement>(null);
-
+ const isEvenIndex = (index:number) => index % 2 === 0;
  useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
@@ -31,6 +30,7 @@ const Hero = () => {
     }
  }, [animatedFeatures]);
 
+
  return (
     <section className="max-container padding-container flex flex-col gap-20 py-16 md:gap-28 lg:py-40 relative" itemScope itemType="http://schema.org/WebPage">
       <div>
@@ -51,21 +51,51 @@ const Hero = () => {
       </div>
 
       <div className="relative z-10">
-        <div className="absolute inset-0 opacity-50 z-[-1] bg-no-repeat"  style={{ backgroundImage: `url('bgImgFooter.png')`, backgroundSize: 'auto 100% '}}/>
+        <div className="absolute inset-0 bg-hero-img opacity-50 z-[-1] bg-no-repeat" />
         <ul ref={ulRef} itemProp="mainEntity" itemScope itemType="http://schema.org/ItemList">
           {FEATURES.map((feature, index) => (
-            <li
+            <>
+            {isEvenIndex(index)&&(
+              <div>
+                <li
               key={feature.id}
               id={feature.id}
               className={`feature flex flex-col w-[100%] md:w-[50%] items-center pr-4 pt-4
-                ${animatedFeatures.includes(feature.id) 
-                 ? (index % 2 === 0 ? 'animate-slide-right mt-[200px]' : 'animate-slide-in-left') : ''} `}
+                ${ animatedFeatures.includes(feature.id)  ? 'animate-slide-in-left' : '' } `}
               itemProp="itemListElement"
               itemScope
               itemType="http://schema.org/CreativeWork"
               style={{ 
-                float: index % 2 === 0 ? 'right' : 'left', 
-                height: index % 2 === 0 ? '' : '1200px' 
+                float:  'left', }}
+            >
+              {feature.link && (
+                <Image
+                 src={feature.link}
+                 alt={feature.text}
+                 width={1080}
+                 height={680}
+                 className="mb-4 mx-auto"
+                 itemProp="image"
+                />
+              )}
+              <h2 className="mt-10 mb-5 text-3xl bold-30 lg:bold-52" itemProp="name">{feature.label}</h2>
+              <p className="text-3xl border-b-4 p-4 text-sky-950 mb-5" itemProp="description">
+                {feature.text}
+              </p>
+            </li>
+              </div>
+            )}
+            {!isEvenIndex(index)&&(
+              <div><li
+              key={feature.id}
+              id={feature.id}
+              className={`feature flex flex-col w-[100%] md:w-[50%] items-center pr-4 pt-4
+              ${ animatedFeatures.includes(feature.id)  ? 'animate-slide-in-right' : '' } `}
+              itemProp="itemListElement"
+              itemScope
+              itemType="http://schema.org/CreativeWork"
+              style={{ 
+                float:'right'
               }}
             >
               {feature.link && (
@@ -83,7 +113,9 @@ const Hero = () => {
                 {feature.text}
               </p>
             </li>
-          ))}
+              </div>
+            )}
+         </> ))}
         </ul>
       </div>
     </section>
